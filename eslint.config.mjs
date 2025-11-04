017c5227d19a0254c607fadf8634dad343e58800
Dimir Abrokov <abrokovdimir@10.215.220.183-vpn.dhcp.yndx.net>
2025-11-04 20:25:53 +0300

    add shadCn, adjust eslint, add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,53 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+/* eslint-disable import/no-unresolved */
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    ...importPlugin.flatConfigs.recommended,
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      ...importPlugin.flatConfigs.recommended.rules,
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        typescript: true,
+      },
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+
+  prettierPlugin,
 ]);
 
 export default eslintConfig;
